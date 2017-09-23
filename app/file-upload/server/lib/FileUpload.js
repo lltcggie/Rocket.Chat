07b4f25734c4b8f2fa01a9b0f96fc20ab5562343
lltcggie <lltcggie@gmail.com>
2017-09-23 14:51:46 +0900

    アップロード時にjpeg以外は画像ファイル変換を行わないようにした
    # Conflicts:
    #	packages/rocketchat-file-upload/server/lib/FileUpload.js

--- app/file-upload/server/lib/FileUpload.js
+++ app/file-upload/server/lib/FileUpload.js
@@ -252,7 +252,7 @@ export const FileUpload = {
 	},
 
 	uploadsOnValidate(file) {
-		if (!/^image\/((x-windows-)?bmp|p?jpeg|png)$/.test(file.type)) {
+		if (!/^image\/(p?jpeg)$/.test(file.type)) {
 			return;
 		}
 
