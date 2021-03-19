bc523d3e8aba473b6d6934b71c1703cf52214e03
lltcggie <lltcggie@gmail.com>
2021-03-20 06:10:58 +0900

    Windowsでmeteor npm startが動くように修正

--- packages/rocketchat-livechat/plugin/build.bat
+++ packages/rocketchat-livechat/plugin/build.bat
@@ -1,21 +1,23 @@
 @echo off
 
-SET NODE_ENV="production"
-cd packages/rocketchat-livechat/.app
-call meteor npm install --production
-call meteor build --headless --directory .meteor/build/
+SET NODE_ENV=production
+SET LIVECHAT_DIR=public\livechat
+SET LIVECHAT_ASSETS_DIR=private\livechat
 
-SET LIVECHAT_DIR="../../../public/livechat"
-SET BUILD_DIR=".meteor/build/bundle/programs/web.browser"
+SET ROOT=%CD%
 
-mkdir ..\public
-del /q /s ..\public\*
-
-rmdir /q /s %LIVECHAT_DIR%
+del /q /s %LIVECHAT_DIR%
 mkdir %LIVECHAT_DIR%
 
-xcopy /y %BUILD_DIR%\*.css %LIVECHAT_DIR%\livechat.css*
-xcopy /y %BUILD_DIR%\*.js %LIVECHAT_DIR%\livechat.js*
-xcopy /y %BUILD_DIR%\head.html ..\public\head.html*
+del /q /s %LIVECHAT_ASSETS_DIR%
+mkdir %LIVECHAT_ASSETS_DIR%
 
-rmdir /s /q .meteor\build\
+echo Installing Livechat %LATEST_LIVECHAT_VERSION%...
+cd %LIVECHAT_DIR%
+
+copy /y %ROOT%\node_modules\@rocket.chat\livechat\build\* .
+
+call meteor node -e "fs.writeFileSync(^"index.html^", fs.readFileSync(^"index.html^").toString().replace(^"<!DOCTYPE^", ^"<!doctype^"));"
+
+cd %ROOT%\%LIVECHAT_ASSETS_DIR%
+copy ..\..\public\livechat\index.html .
